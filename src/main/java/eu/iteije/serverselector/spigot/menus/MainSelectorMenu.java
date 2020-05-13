3e3d1528566ed06a354cc892ff9b31038a7004a8
iTeije <iteije@outlook.com>
2020-05-13 15:32:17 +0200

    Just created some files for main and sub menu.. not working yet

---
+++ src/main/java/eu/iteije/serverselector/spigot/menus/MainSelectorMenu.java
@@ -0,0 +1,13 @@
+package eu.iteije.serverselector.spigot.menus;
+
+import eu.iteije.serverselector.spigot.ServerSelectorSpigot;
+
+public class MainSelectorMenu {
+
+    private ServerSelectorSpigot serverSelectorSpigot;
+
+    public MainSelectorMenu(ServerSelectorSpigot instance) {
+        this.serverSelectorSpigot = instance;
+    }
+
+}
---
+++ src/main/java/eu/iteije/serverselector/spigot/menus/SubSelectorMenu.java
@@ -0,0 +1,13 @@
+package eu.iteije.serverselector.spigot.menus;
+
+import eu.iteije.serverselector.spigot.ServerSelectorSpigot;
+
+public class SubSelectorMenu {
+
+    private ServerSelectorSpigot serverSelectorSpigot;
+
+    public SubSelectorMenu(ServerSelectorSpigot instance) {
+        this.serverSelectorSpigot = instance;
+    }
+
+}
